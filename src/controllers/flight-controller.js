9f12a27f7a8cf624e4d05961949b372dcb768943
guptavarun619 <guptavarun619@gmail.com>
2022-12-30 14:25:43 +0530

    [FEAT] Added APIs for updating and fetching flight by id(PK)

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -49,7 +49,47 @@ const getAll = async (req, res) => {
   }
 };
 
+const get = async (req, res) => {
+  try {
+    const flight = await flightService.getFlight(req.params.id);
+    return res.status(SuccessCodes.OK).json({
+      data: flight,
+      success: true,
+      message: "Successfully fetched flight data",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      data: {},
+      success: false,
+      message: "Unable to fetch flight data",
+      err: error,
+    });
+  }
+};
+
+const update = async (req, res) => {
+  try {
+    const flight = await flightService.updateFlight(req.params.id, req.body);
+    return res.status(SuccessCodes.OK).json({
+      data: flight,
+      success: true,
+      message: "Successfully updated flight data",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      data: {},
+      success: false,
+      message: "Unable to update flight data",
+      err: error,
+    });
+  }
+};
+
 module.exports = {
   create,
   getAll,
+  get,
+  update,
 };
